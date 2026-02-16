3ff2ea39b06ead07451dda21511d99c50a5410f1
Josue Igiraneza <josue.igiraneza@bestseller.com>
2026-02-16 10:37:10 +0200

    add routing to about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,12 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
-  {
-    path: '',
-    component: TasksComponent,
-  },
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
@@ -26,6 +26,8 @@ const appRoutes: Routes = [
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
