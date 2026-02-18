1351fb7d311a44751f4783125cf1898d94d875af
Josue Igiraneza <josue.igiraneza@bestseller.com>
2026-02-18 16:48:15 +0200

    create task details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
 
 const appRoutes: Routes = [
   { path: '', component: TasksComponent },
@@ -28,6 +29,7 @@ const appRoutes: Routes = [
     AddTaskComponent,
     AboutComponent,
     FooterComponent,
+    TaskDetailsComponent,
   ],
   imports: [
     BrowserModule,
