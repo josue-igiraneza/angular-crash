1ec2ef5ec78467f9100cdece7351c22b664d185d
Josue Igiraneza <josue.igiraneza@bestseller.com>
2026-02-19 17:11:23 +0200

    create back button component with styling and functionality

--- src/app/components/back-button/back-button.component.ts
+++ src/app/components/back-button/back-button.component.ts
@@ -1,15 +1,17 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-back-button',
   templateUrl: './back-button.component.html',
-  styleUrls: ['./back-button.component.css']
+  styleUrls: ['./back-button.component.css'],
 })
 export class BackButtonComponent implements OnInit {
+  constructor(private location: Location) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick() {
+    this.location.back();
   }
-
 }
